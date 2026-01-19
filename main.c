9c36ab8103fc698d313b81e5edcfc4880825328b
Selim Efe <uysalselimefe@gmail.com>
2026-01-19 15:38:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,19 @@
 
 int main()
 {
-        int i,j,k,satir;
-    satir=5;
+    int i, j, k, satir;
+    satir = 5;
 
-    for(i=1;i<=satir;i++){
-            for(k=1;k<=satir-i;k++){
-                printf(" ");
-            }
-        for(j=1;j<=2*i-1;j++){
+    for (i = 1; i <= satir; i++)
+    {
+        for (k = 1; k <= satir - i; k++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
             printf("*");
         }
-            printf("\n");
+        printf("\n");
     }
 }
